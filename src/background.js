a1934d9eea8134d4eedfa53a7bfe99339aed9cd8
Cloakd <2733949+cloakd@users.noreply.github.com>
2021-09-30 07:47:24 +0100

    adjust for CMC new button format ;)

--- src/background.js
+++ src/background.js
@@ -1,12 +1,13 @@
 function checkDiamondClaim() {
     console.log("Checking claim");
-    chrome.storage.local.get(["cmc.diamond.timeout"], function (data) {
+    chrome.storage.sync.get(["cmc.diamond.timeout"], function (data) {
 
         var left = (data["cmc.diamond.timeout"] - Date.now()) / 1000;
-        console.log("LEFT",left);
-        if (left <= 10)
+        if (left <= 10) {
             chrome.browserAction.setBadgeText({text: 'CLAIM'});
             chrome.browserAction.setBadgeBackgroundColor({color: 'red'});
+        } else
+            chrome.browserAction.setBadgeText({text: ''});
     });
 }
 
--- src/content.js
+++ src/content.js
@@ -1,27 +1,28 @@
 (function () {
     "use strict";
 
-    var buttons = document.getElementsByTagName("button");
-    console.log("Button: ", buttons);
-    for (var i = 0; i < buttons.length; i++) {
-        console.log("Button: ", buttons[i]);
+    var re = /[0-5].+:[0-5].+:[0-5]./;
 
+    var buttons = document.getElementsByTagName("button");
+    for (var i = 0; i < buttons.length; i++) {
         if (!buttons[i].innerText.toLowerCase().indexOf("diamonds"))
             continue; //Skip
-
         buttons[i].onclick = handleClaim
     }
 
     function handleClaim(tab) {
         var text = tab.target.innerHTML;
+        var res = text.match(re);
+        console.log(res[0]);
+
         if (text.indexOf("collect")) {
-            var tm = text.split(" ");
+            // var split = tm[0].split(":");
+            var split = res[0].split(":");
 
-            console.log("Setting timeout", tm);
+            // var tm = text.split(" ");
+            console.log("Split", split);
 
-            var split = tm[0].split(":");
-
-            var total = (split[0]*3600) + (split[1]*60) + (split[2]*1);
+            var total = (split[0] * 3600) + (split[1] * 60) + (split[2] * 1);
             console.log("asd", {
                 1: split[0],
                 2: split[1],
@@ -29,10 +30,9 @@
             });
             console.log('Total Seconds: ', total);
 
-            var redeemAt = Date.now() + (total*1000);
+            var redeemAt = Date.now() + (total * 1000);
 
-            chrome.storage.local.set({"cmc.diamond.timeout": redeemAt});
-            chrome.browserAction.setBadgeText({text: ''});
+            chrome.storage.sync.set({"cmc.diamond.timeout": redeemAt});
             return;
         }
         //TODO
